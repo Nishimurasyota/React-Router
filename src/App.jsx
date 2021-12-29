3e6343e61c8eac429b1a2169c1d37c0a45b12cf5
Nishimurasyota <59325211+Nishimurasyota@users.noreply.github.com>
2021-12-29 19:28:54 +0900

    Fix-Route Basic Route setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Routes, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -9,10 +9,32 @@ export const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <Home />
-        <Page1 />
-        <Page2 />
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">Page1</Link>
+        <br />
+        <Link to="/Page2">Page2</Link>
       </div>
+
+      {/** Version5.3の時の記述方法 */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Page1">
+          <Page1 />
+        </Route>
+        <Route path="/Page2">
+          <Page2 />
+        </Route>
+      </Switch>
+
+      {/* Version6以降の記述方法 */}
+      {/* <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/Page2" element={<Page2 />} />
+      </Routes> */}
     </BrowserRouter>
   );
 };
